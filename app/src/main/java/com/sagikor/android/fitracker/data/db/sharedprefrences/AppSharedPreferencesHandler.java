b302d78707db75d05c579a87f321cc0fb92fac9c
Sagi Korzack <Sagi1193@gmail.com>
2021-04-02 20:17:02 +0300

    mvp architecture: sharedPreferences handler

--- app/src/main/java/com/sagikor/android/fitracker/data/db/sharedprefrences/AppSharedPreferencesHandler.java
+++ app/src/main/java/com/sagikor/android/fitracker/data/db/sharedprefrences/AppSharedPreferencesHandler.java
@@ -1,4 +1,44 @@
 package com.sagikor.android.fitracker.data.db.sharedprefrences;
 
+
+import android.content.SharedPreferences;
+
 public class AppSharedPreferencesHandler implements SharedPreferencesHandler {
+    private final static AppSharedPreferencesHandler sharedPreferencesHandler = init();
+    private SharedPreferences sharedPreferences;
+
+    private static AppSharedPreferencesHandler init() {
+        return new AppSharedPreferencesHandler();
+    }
+
+    public static AppSharedPreferencesHandler getInstance(){
+        return sharedPreferencesHandler;
+    }
+
+    @Override
+    public void setSharedPreferences(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    @Override
+    public boolean isGirlsSwitchOn() {
+        return sharedPreferences.getBoolean("alwaysGirlsSwitch", false);
+
+    }
+
+    @Override
+    public boolean isBoysSwitchOn() {
+        return sharedPreferences.getBoolean("alwaysBoysSwitch", false);
+    }
+
+    @Override
+    public void editGenderPreferences(String gender,boolean isChecked) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if(gender.equals("Girls")){
+            editor.putBoolean("alwaysGirlsSwitch", isChecked);
+        }else if(gender.equals("Boys")){
+            editor.putBoolean("alwaysBoysSwitch", isChecked);
+        }
+        editor.apply();
+    }
 }
--- app/src/main/java/com/sagikor/android/fitracker/data/db/sharedprefrences/SharedPreferencesHandler.java
+++ app/src/main/java/com/sagikor/android/fitracker/data/db/sharedprefrences/SharedPreferencesHandler.java
@@ -1,4 +1,14 @@
 package com.sagikor.android.fitracker.data.db.sharedprefrences;
 
+import android.content.SharedPreferences;
+
 public interface SharedPreferencesHandler {
+
+    void setSharedPreferences(SharedPreferences sharedPreferences);
+
+    boolean isGirlsSwitchOn();
+
+    boolean isBoysSwitchOn();
+
+    void editGenderPreferences(String gender,boolean isChecked);
 }
