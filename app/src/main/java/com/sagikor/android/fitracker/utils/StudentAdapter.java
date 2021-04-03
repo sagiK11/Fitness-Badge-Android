cb86cbefb0346c15fdfa8f9d2a5bc4b478580e24
Sagi Korzack <Sagi1193@gmail.com>
2021-04-03 17:48:18 +0300

    fix: remove context being passed to the adapter

--- app/src/main/java/com/sagikor/android/fitracker/utils/StudentAdapter.java
+++ app/src/main/java/com/sagikor/android/fitracker/utils/StudentAdapter.java
@@ -1,50 +1,115 @@
 package com.sagikor.android.fitracker.utils;
 
-import android.app.Activity;
+
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.sagikor.android.fitracker.R;
 import com.sagikor.android.fitracker.data.model.Student;
+import com.sagikor.android.fitracker.ui.contracts.ViewStudentsActivityContract;
 
 import java.util.List;
 
-public class StudentAdapter extends ArrayAdapter<Student> {
-    private Context context;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
-    public StudentAdapter(Activity context, List<Student> studentsList) {
-        super(context, 0, studentsList);
-        this.context = context;
+public class StudentAdapter extends  RecyclerView.Adapter<StudentAdapter.CustomViewHolder> {
+    private static final String TAG = "StudentAdapter";
+    private List<Student> list;
+    private final ViewStudentsActivityContract.Presenter presenter;
+
+    public StudentAdapter( ViewStudentsActivityContract.Presenter presenter){
+        this.list = presenter.getStudentsList();
+        this.presenter = presenter;
     }
 
-    public View getView(int position, View listItemView, ViewGroup parent) {
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.student_list_item, parent, false);
-        }
-
-        Student currentStudent = this.getItem(position);
-
-        TextView studentName = listItemView.findViewById(R.id.student_name_view_data_id);
-        studentName.setText(currentStudent.getName());
-
-        TextView studentClass = listItemView.findViewById(R.id.student_class_view_data_id);
-        studentClass.setText(currentStudent.getStudentClass());
-
-        ImageView studentAvatar = listItemView.findViewById(R.id.avatar_image_view);
-
-        if (currentStudent.getGender().equals(context.getResources().getString(R.string.girl))) {
-            studentAvatar.setImageDrawable(context.getResources().getDrawable(R.mipmap.female_athlete_avatar, null));
-        } else {
-            studentAvatar.setImageDrawable(context.getResources().getDrawable(R.mipmap.male_avatar, null));
-        }
-
-        return listItemView;
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.student_list_item,parent,false);
+        return new CustomViewHolder(view);
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        Student student = list.get(position);
+        holder.tvName.setText(student.getName());
+        holder.tvClass.setText(student.getStudentClass());
+        String female =  holder.ivAvatar.getContext().getResources().getString(R.string.girl);
+        if(list.get(position).getGender().equals(female))
+            holder.ivAvatar.setImageResource(R.mipmap.female_athlete_avatar);
+        else
+            holder.ivAvatar.setImageResource(R.mipmap.male_avatar);
+        setStudentOnClickListener(holder,position);
+
+    }
+
+    private void setStudentOnClickListener(CustomViewHolder holder,int position) {
+        Student student = list.get(position);
+        Context context = holder.ivAvatar.getContext();
+        final String YES = context.getString(R.string.yes);
+        final String NO = context.getResources().getString(R.string.no);
+        final String DELETE_STUDENT_QUESTION = context.getResources().getString(R.string.delete_student_question);
+        //short click
+        holder.itemView.setOnClickListener(e -> {
+            presenter.onStudentClick(position);
+        });
+        //long click
+        holder.itemView.setOnLongClickListener(e->{
+            new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText(DELETE_STUDENT_QUESTION)
+                    .setConfirmText(YES)
+                    .setConfirmClickListener(sDialog -> {
+                        sDialog.dismissWithAnimation();
+                        presenter.deleteStudent(student);
+                        deleteItem(position);
+                    })
+                    .setCancelButton(NO,
+                            SweetAlertDialog::dismissWithAnimation)
+                    .show();
+            return true;
+        });
+
+    }
+
+    private void deleteItem(int position) {
+        list.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, list.size());
+    }
+
+    public void updateList(List<Student> list){
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    static class CustomViewHolder extends RecyclerView.ViewHolder{
+        public final View view;
+
+        private final TextView tvName;
+        private final TextView tvClass;
+        private final ImageView ivAvatar;
+
+        public CustomViewHolder(View view){
+            super(view);
+            this.view = view;
+
+            tvName = view.findViewById(R.id.student_name_view_data_id);
+            tvClass = view.findViewById(R.id.student_class_view_data_id);
+            ivAvatar = view.findViewById(R.id.avatar_image_view);
+        }
+    }
 }
