768cbe1a80eba4d6544d80ce63414efd30e53c6f
Sagi Korzack <Sagi1193@gmail.com>
2021-04-05 14:46:40 +0300

    add: list view divider

--- app/src/main/java/com/sagikor/android/fitracker/ui/view/ViewStudentsActivity.java
+++ app/src/main/java/com/sagikor/android/fitracker/ui/view/ViewStudentsActivity.java
@@ -5,12 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.sagikor.android.fitracker.R;
 import com.sagikor.android.fitracker.ui.contracts.ViewStudentsActivityContract;
@@ -22,6 +26,7 @@ public class ViewStudentsActivity extends AppCompatActivity implements ViewStude
     private StudentAdapter adapter;
     private RecyclerView listView;
     private EditText inputSearch;
+    private ProgressBar progressBar;
     private ViewStudentsActivityContract.Presenter presenter;
 
 
@@ -61,11 +66,29 @@ public class ViewStudentsActivity extends AppCompatActivity implements ViewStude
     private void bindViews() {
         listView = findViewById(R.id.list_view);
         inputSearch = findViewById(R.id.search_input);
+        progressBar = findViewById(R.id.view_students_progress_bar);
+        listView.addItemDecoration(new DividerItemDecoration(listView.getContext(),
+                DividerItemDecoration.VERTICAL));
     }
 
     @Override
     public void navToStudentUpdate() {
         startActivity(new Intent(this, UpdateStudentActivity.class));
+    }
+
+    @Override
+    public void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgressBar() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void notifyAdapterDataChanged() {
+        adapter.notifyDataSetChanged();
     }
 
     private void addInputSearch() {
