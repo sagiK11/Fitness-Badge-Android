b890e0f06c5cdef17984db0b049928c0fce72839
Sagi Korzack <Sagi1193@gmail.com>
2021-04-06 18:57:00 +0300

    add: signin & register logic

--- app/src/main/java/com/sagikor/android/fitracker/ui/contracts/SignInActivityContract.java
+++ app/src/main/java/com/sagikor/android/fitracker/ui/contracts/SignInActivityContract.java
@@ -1,7 +1,7 @@
 package com.sagikor.android.fitracker.ui.contracts;
 
 public interface SignInActivityContract {
-    interface Presenter {
+    interface Presenter extends BaseContract.BasePresenter {
         void onLoginClick();
 
         void onRegisterClick();
@@ -11,9 +11,36 @@ public interface SignInActivityContract {
         void bind(SignInActivityContract.View view);
 
         void unbind();
+
+        void onStart();
     }
 
-    interface View {
+    interface View extends BaseContract.BaseView {
+        void navToHomeScreen();
+
+        void navSignedUserToHomeScreen();
+
+        void navToRegisterScreen();
+
+        void showProgressBar();
+
+        void hideProgressBar();
+
+        void setErrorInEmail();
+
+        void setErrorInPassword();
+
+        String getUserEmail();
+
+        String getUserPassword();
+
+        void popError(String error);
+
+        void popSignInFailureMessage();
+
+        void notifyUserToCheckMail();
+
+        void notifyUserToFillEmail();
 
     }
 }
